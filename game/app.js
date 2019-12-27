fc3e5200dbcc4cee3525fa7a4599eb8477cebaff
Andy <andreydmitrievich666@gmail.com>
2019-12-27 12:03:36 +0300

    update

---
+++ game/app.js
@@ -0,0 +1,57 @@
+import * as THREE from 'three';
+import Controls from './controls.js';
+
+export default class Game {
+    constructor(rootElement){
+        this.control = new Controls();
+
+        this.scene =  new THREE.Scene();
+
+        this.renderer = new THREE.WebGLRenderer();
+        this.renderer.setPixelRatio(window.devicePixelRatio);
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+
+        this.camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.1, 1000 );
+        this.camera.position.set( 0, 0, 10 );
+        this.camera.lookAt( 0, 0, 0 );
+
+        this.container = rootElement;
+        this.container.appendChild(this.renderer.domElement);
+
+        this.objects = {}
+
+        this.resize();
+        window.addEventListener('resize', this.resize.bind(this)); 
+
+        this.addObjects();
+
+        this.run();
+    }
+
+    resize(){
+        var w = window.innerWidth;
+        var h = window.innerHeight;
+        this.renderer.setSize( w, h );
+        this.camera.aspect = w / h;
+        this.camera.updateProjectionMatrix();
+    }
+
+    addObjects() {
+        const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+        const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+        this.objects.cube = new THREE.Mesh( geometry, material );
+        this.scene.add( this.objects.cube );
+    }
+
+    run(time) {
+        requestAnimationFrame((time) => this.run(time));
+
+        // this.objects.cube.rotation.x += 0.01;
+        // this.objects.cube.rotation.y += 0.01;
+
+        this.renderer.render( this.scene, this.camera );
+    }
+}
+
+const game = new Game(document.getElementById('game'));
+// game.run();
